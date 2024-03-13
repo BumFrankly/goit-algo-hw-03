2c49ab65b522ae0c401317f5c2d7a860f879fbed
Dmytro Shepeliuk <60881355+BumFrankly@users.noreply.github.com>
2024-03-13 20:57:00 +0200

    Update task1.py

--- goit-algo-hw-03/task1.py
+++ goit-algo-hw-03/task1.py
@@ -1,22 +1,20 @@
 from datetime import datetime
 
 def get_days_from_today(date):
+    try:
+        date_obj = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        print("Неправильний формат дати. Потрібно YYYY-MM-DD.")
+        return None
 
-  try:
-    date_obj = datetime.strptime(date, '%Y-%m-%d')
-  except ValueError:
-    raise ValueError("Неправильний формат дати. Потрібно YYYY-MM-DD.")
+    today = datetime.today()
 
-  today = datetime.today()
+    difference = today - date_obj
+    days_from_today = difference.days
 
-  difference = today - date_obj
-  days_from_today = difference.days
+    return days_from_today
 
-  return days_from_today
-
-try:
-  date = '2020-10-09'
-  days_from_today = get_days_from_today(date)
-  print(f"Кількість днів від {date} до сьогодні: {days_from_today}")
-except ValueError as e:
-  print(e)
+date = '2020-10-09'
+days_from_today = get_days_from_today(date)
+if days_from_today is not None:
+    print(f"Кількість днів від {date} до сьогодні: {days_from_today}")
