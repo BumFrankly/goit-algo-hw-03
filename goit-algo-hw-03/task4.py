1bcdd6b6ea8fd0e03f9045b32288f8aa6dfb000d
Dmytro Shepeliuk <60881355+BumFrankly@users.noreply.github.com>
2024-03-13 20:52:04 +0200

    Update task4.py

--- goit-algo-hw-03/task4.py
+++ goit-algo-hw-03/task4.py
@@ -1,35 +1,40 @@
 from datetime import datetime, timedelta
 
 def get_upcoming_birthdays(users):
-  upcoming_birthdays = []
-  today = datetime.today().date()
+    upcoming_birthdays = []
+    today = datetime.today().date()
 
-  for user in users:
-    birthday_this_year = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
-    birthday_next_year = birthday_this_year.replace(year=today.year + 1)
+    for user in users:
+        birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
+        
+        next_birthday = birthday.replace(year=today.year)
+        if next_birthday < today:
+            next_birthday = next_birthday.replace(year=today.year + 1)
 
-    days_until_birthday = (birthday_this_year - today).days
-    if days_until_birthday < 0:
-      days_until_birthday = (birthday_next_year - today).days
+        days_until_birthday = (next_birthday - today).days
+        if 0 <= days_until_birthday <= 7:
+            upcoming_birthdays.append({
+                "name": user["name"],
+                "congratulation_date": next_birthday.strftime("%Y.%m.%d")
+            })
 
-    if days_until_birthday <= 7:
-      congratulation_date = birthday_this_year
+    return upcoming_birthdays
 
-      if birthday_this_year.weekday() == 5: 
-        congratulation_date += timedelta(days=2) 
-
-      upcoming_birthdays.append({
-        "name": user["name"],
-        "congratulation_date": congratulation_date.strftime("%Y.%m.%d")
-      })
-
-  return upcoming_birthdays
-
-users = [
-  {"name": "Олена", "birthday": "2024.03.13"},
-  {"name": "Вікторія", "birthday": "2024.03.16"}
-]
+users = [{"name": "Jane Smith", "birthday": "1990.03.10"},
+        {"name": "Nick Darsel", "birthday": "1984.03.11"},
+        {"name": "Liam Smith", "birthday": "1995.03.12"},
+	{"name": "Mohel Smith", "birthday": "1995.03.13"},
+        {"name": "John Dark", "birthday": "1985.03.14"},
+        {"name": "Mary Dark", "birthday": "1985.03.15"},
+        {"name": "Derek Dark", "birthday": "1985.03.16"},
+        {"name": "Jane Smith", "birthday": "1990.03.17"}, 
+        {"name": "Liam Smith", "birthday": "1995.03.18"},
+	{"name": "Mohel Smith", "birthday": "1995.03.19"},
+        {"name": "John Dark", "birthday": "1985.03.20"},
+        {"name": "Mary Dark", "birthday": "1985.03.21"},
+        {"name": "Derek Dark", "birthday": "1985.03.22"},
+        {"name": "Jane Smith", "birthday": "1990.03.23"}]
 
 upcoming_birthdays = get_upcoming_birthdays(users)
-
 print("Список привітань на цьому тижні:", upcoming_birthdays)
+
